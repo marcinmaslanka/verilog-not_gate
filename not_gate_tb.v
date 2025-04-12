9f1c479a68514f8446fd8937276cb3d2f04bed4e
Marcin Maślanka <159284726+marcinmaslanka@users.noreply.github.com>
2025-04-12 13:21:18 +0200

    Create not_gate_tb.v

---
+++ not_gate_tb.v
@@ -0,0 +1,20 @@
+`include "not.v"
+
+module not_tb();
+    reg a;
+    wire y;
+
+    not_gate uut(a,y);
+
+    initial begin
+
+    $dumpfile("not_tb.vcd");
+    $dumpvars(0, not_tb);
+    
+    a=1'b0;
+    #100 a=1'b1;
+    #100 a=1'b0;
+    
+    end
+
+endmodule
